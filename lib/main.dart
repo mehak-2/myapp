9d8253487befcb45e08219b2b059490e0d2594fe
mehak <kangmehak167@gmail.com>
2024-02-20 19:56:09 +0530

    dev#ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main(){
   runApp(MaterialApp(
     title: "Myapp",
     home: HomePage(),
-  ));
+  )); 
 }
 
 class HomePage extends StatelessWidget {
@@ -14,9 +14,68 @@ class HomePage extends StatelessWidget {
      appBar: AppBar(
       title: Text("Myapp"),
      ),
-     body: Container(
-      child: Center(child: Text("Hi Flutter")),
+body: Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Align(alignment: Alignment.bottomRight,
+    child:Container(
+      color: Colors.amber,
+      width: 200,
+      height: 400,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Column(
+        
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        padding: const EdgeInsets.all(8),
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+       Container(
+        padding: const EdgeInsets.all(8),
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+       Container(
+        padding: const EdgeInsets.all(8),
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+
+    ]
+    
+    // decoration: BoxDecoration(
+    //   borderRadius: BorderRadius.circular(10),
+    //   boxShadow: [
+    //     BoxShadow(
+    //       color: Colors.grey,
+    //       blurRadius: 5,
+    //       offset: Offset(2.0, 5.0)
+    //     )
+    //   ],
+    //   color: Colors.teal,
+    //   gradient: LinearGradient(
+    //     colors: [Colors.yellow, Colors.pink],
+    //   )
+    //  ),
+    //  child: Text(
+    //   "I am a Box",
+    //  style: TextStyle(
+    //   color: Colors.green,
+    //   fontWeight: FontWeight.bold,
+    //   fontSize: 20,
      ),
-    );
+     ),
+    ),
+    ),
+),
+     );
+    
+
 }
 }
